919e61d9dd3fffbcff247e256886363e10910f73
Ravirbikant <ravirbikant@gmail.com>
2016-10-14 17:03:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,85 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one':{
-    title: 'Article One | Ravi Kant Jha',
-    heading: 'Article One',
-    date: 'Oct 13, 2016',
-    content: `
-             <p>
-                This is the first article and in the next step I will allign this web-page to my home page. `
-                
-},
-    'article-two':{
-    title: 'Article Two | Ravi Kant Jha',
-    heading: 'Article Two',
-    date: 'Oct 13, 2016',
-    content: `
-             <p>
-                This is the second article and in the next step I will allign this web-page to my home page. `
-               },
-    'article-three':{
-    title: 'Article Three | Ravi Kant Jha',
-    heading: 'Article Three',
-    date: 'Oct 13, 2016',
-    content: `
-             <p>
-                This is the third article and in the next step I will allign this web-page to my home page. `
-               },
-};
-
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-    var htmlTemplate =`
-    <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            <p>
-             ${content}
-        </div>
-        </div>
-    </body>
-    </html>
-`;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function(req, res){
-    // articleName== article-one
-    // articles[articleName]=={} content object for article one
-    var articleName = req.params.aricleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/article-one', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-two', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
