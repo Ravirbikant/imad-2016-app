88b98399e3ff58edc76132c552c63f96b8f58760
Ravirbikant <ravirbikant@gmail.com>
2016-10-14 16:52:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/:articleName', function(req, res){
     // articleName== article-one
     // articles[articleName]=={} content object for article one
     var articleName = req.params.aricleName;
-  res.send(createtemplate(articles[articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
