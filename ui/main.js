a16ed553a5755d9361d18233c2ccbf0fcffb989c
Ravirbikant <ravirbikant@gmail.com>
2016-10-19 15:54:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,25 @@
-console.log('Loaded!');
-// Move the Image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft +  'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+// Counter code
+var button= document.getElementById('counter');
+
+button.onclick = function (){
+    
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status === 200){
+                var conter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not Done yet
+    };
+    
+    //Make the request
+    request.open('GET', 'http://http://ravirbikant.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
